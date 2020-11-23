58ea8edccd931bf0dfdd42cecd54211c1e57b21d
Ahsan Jamil <57091356+ahsanjamil306@users.noreply.github.com>
2020-11-24 00:12:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>HELLO WORLD</h1> 
+     <h1>HELLO WORLD</h1> Hello world!
     </div>
   );
 }
